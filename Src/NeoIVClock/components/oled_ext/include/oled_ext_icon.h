d2e00e3cb1cc4f970ea48c7aa97359f5f8362942
steelm <steelm@madcat.cc>
2026-05-26 16:56:09 +0800

    删除了一些垃圾代码，由于实现了汉字自由，不用画ICON了

--- Src/NeoIVClock/components/oled_ext/include/oled_ext_icon.h
+++ Src/NeoIVClock/components/oled_ext/include/oled_ext_icon.h
@@ -3,14 +3,6 @@
 
 #include <stdint.h>
 
-/* 40 * 20 */
-extern const uint8_t oled_ext_char_WENDU[];
-extern const uint8_t oled_ext_char_SHIDU[];
-extern const uint8_t oled_ext_char_PRESS[];
-extern const uint8_t oled_ext_char_JIAQUAN[];
-extern const uint8_t oled_ext_char_PM25[];
-extern const uint8_t oled_ext_char_TVOC[];
-
 /* 30 * 20 */
 extern const uint8_t oled_ext_char_ATM[];
 extern const uint8_t oled_ext_char_HGM[];
